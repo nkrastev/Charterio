3d541e704288cb548a8b8b4a8d700b511a5ceec5
Nikolay Krastev <nikolay.krystev@gmail.com>
2022-02-26 23:19:35 +0200

    Minor changes on Home Page

---
+++ Web/Charterio.Web/Areas/Administration/Controllers/TicketController.cs
@@ -0,0 +1,17 @@
+﻿namespace Charterio.Web.Areas.Administration.Controllers
+{
+    using Charterio.Common;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    [Area("Administration")]
+
+    public class TicketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
