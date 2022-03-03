512a51f02a517f15e7f29d6ea57319085ac5df05
Nikolay Krastev <nikolay.krystev@gmail.com>
2022-03-03 18:46:08 +0200

    Bug found > Test created.

---
+++ Tests/Charterio.Services.Data.Tests/FlightServiceTests.cs
@@ -0,0 +1,44 @@
+﻿namespace Charterio.Services.Data.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Charterio.Data;
+    using Charterio.Data.Models;
+    using Charterio.Services.Data.Flight;
+    using Microsoft.EntityFrameworkCore;
+    using Moq;
+    using Xunit;
+
+    public class FlightServiceTests
+    {
+        // Bugs found > tests created
+        [Fact]
+        public void IfTargetOfferIsInactiveInWeb_GetFlightById_ReturnsNull()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase("Database_For_Tests_Flight").Options;
+            var dbContext = new ApplicationDbContext(options);
+            var allotmentService = new AllotmentService(dbContext);
+            var flightService = new FlightService(dbContext, allotmentService);
+
+            var offer = new Offer
+            {
+                Name = "TestOffer",
+                FlightId = 1,
+                StartAirportId = 1,
+                EndAirportId = 1,
+                StartTimeUtc = DateTime.UtcNow,
+                EndTimeUtc = DateTime.UtcNow.AddSeconds(1),
+                Price = 1,
+                CurrencyId = 1,
+                AllotmentCount = 1,
+                IsActiveInWeb = false,
+            };
+            dbContext.Offers.Add(offer);
+            Assert.Null(flightService.GetById(1));
+        }
+    }
+}
